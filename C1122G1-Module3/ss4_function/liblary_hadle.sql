64a4fce668b2b2d651b31ffcc004785630584245
khainlv99 <117066707+khainlv99@users.noreply.github.com>
2023-02-28 07:58:19 +0700

    update

--- C1122G1-Module3/ss4_function/liblary_hadle.sql
+++ C1122G1-Module3/ss4_function/liblary_hadle.sql
@@ -9,40 +9,40 @@ GROUP BY book_id
 ORDER BY muon DESC) as max;
 
 -- Thống kê các đầu sách chưa từng được mượn--
-select books.b_name,books.b_id,count(books.b_id) as muon 
+select books.name,books.id,count(books.id) as muon 
 from borrows
-join books on books.b_id = borrows.book_id
-group by books.b_id
+join books on books.id = borrows.book_id
+group by books.id
 having muon = (
-select count(books.b_id) as muon from borrows
-join books on books.b_id = borrows.book_id
-group by books.b_id
+select count(books.id) as muon from borrows
+join books on books.id = borrows.book_id
+group by books.id
 order by muon desc limit 0
 );
 -- Thống kê các đầu sách chưa được mượn --
-select books.b_name,books.b_id
+select books.name,books.id
 from borrows
-inner join books on books.b_id = borrows.book_id
-where  not exists (select books.b_id where curdate() between borrows.borrow_date and borrows.return_date)
-group by books.b_id;
+inner join books on books.id = borrows.book_id
+where  not exists (select books.id where curdate() between borrows.borrow_date and borrows.return_date)
+group by books.id;
 
-select books.b_name,books.b_id
+select books.name,books.id
 from books
-left join borrows on books.b_id=borrows.book_id 
+left join borrows on books.id=borrows.book_id 
 where borrows.book_id not in (select borrows.book_id from borrows where curdate() between borrows.borrow_date and borrows.return_date)
-group by books.b_id;
+group by books.id;
 
 -- Lấy ra danh sách các học viên đã từng mượn sách và sắp xếp  theo số lượng mượn sách từ lớn đến nhỏ					
-select s_name, count(borrows.id) as muon 
+select name, count(borrows.id) as muon 
 from students 
-join borrows on students.s_id = borrows.student_id
-where students.s_id
-group by students.s_name
+join borrows on students.id = borrows.student_id
+where students.id
+group by students.name
 order by muon desc;
 
 -- - Lấy ra các học viên mượn sách nhiều nhất của thư viện				
-select s_name, count(borrows.id) as muon
+select name, count(borrows.id) as muon
 from students 
-join borrows on students.s_id = borrows.student_id
-group by students.s_name
+join borrows on students.id = borrows.student_id
+group by students.name
 order by muon desc limit 2;
